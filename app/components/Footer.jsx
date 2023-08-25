933add03a4da5a09ff594fef81ff5af7268cdba9
rafaelrossetti23 <rafaelvinicius2310@gmail.com>
2023-08-25 12:18:39 -0300

    added footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../page.module.css"
+
+const Header = ()=>{
+    return(
+        <footer className={styles.footer}>
+           <p>Todos os direitos reservados a Rafael Rossetti</p>
+        </footer>
+    )}
+export default Header;
