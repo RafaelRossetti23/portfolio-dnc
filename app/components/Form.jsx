5ad3e98764d0033cd7894bed1db2e66ff22ea74a
rafaelrossetti23 <rafaelvinicius2310@gmail.com>
2023-08-25 11:49:14 -0300

    estilizacao da secao entre-em-contato

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -9,10 +9,10 @@ const Form = ()=>{
             </div>
             <div className={styles.form_content}>
                 <p>Economize seu tempo</p>
-                <h2>Ganhe mais dinheiro</h2>
-                <p>Digite aqui seu endereço de e-mail. Eu entrarei em<br/> contato para fornecer um diagnóstico gratuito.</p>
+                <h1>Ganhe mais dinheiro</h1>
+                <p>Digite aqui seu endereço de e-mail. Eu entrarei em contato para fornecer um diagnóstico gratuito.</p>
                 <form action="https://formspree.io/f/xvojavdz" method="post">
-                    <input type="email" name="email" placeholder="Coloque seu E-mail" />
+                    <input type="email" name="email" placeholder="E-mail" />
                     <button type="submit">Enviar</button>
                 </form>
             </div>
